5077bd5fd3c762d5d62986c95220604ab3d3d9d4
Vahid Khalafov <vahid.khalafovv@gmail.com>
2025-11-05 21:35:50 -0800

    Refactor code structure for improved readability and maintainability and add threading to sound.py

--- src/path.py
+++ src/path.py
@@ -3,6 +3,7 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
+SOUNDS_DIR = os.path.join(ASSETS_DIR, "sounds")
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 FACES_DIR = os.path.join(DATA_DIR, "faces")
 DOCS_DIR = os.path.join(ROOT_DIR, "docs")
@@ -12,6 +13,6 @@ SRC_DIR = os.path.join(ROOT_DIR, "src")
 ATTENDANCE_CSV = os.path.join(DATA_DIR, "attendance.csv")
 STUDENTS_CSV = os.path.join(DATA_DIR, "students.csv")
 
-RECOGNIZE_WAV = os.path.join(ASSETS_DIR, "recognize.wav")
+RECOGNIZE_WAV = os.path.join(SOUNDS_DIR, "recognize.wav")
 
 EVENTS_LOG = os.path.join(LOGS_DIR, "events.log")
--- src/sound.py
+++ src/sound.py
@@ -1,10 +1,11 @@
 from log import logger
+import threading
 import playsound
 
 def play(sound):
     logger.debug(f"Playing sound: {sound}")
     try:
-        playsound.playsound(sound)
+        threading.Thread(target=lambda:playsound.playsound(sound), daemon=True).start()
         logger.info(f"Sound played: {sound}")
     except Exception as e:
         logger.error(f"Failed to play sound {sound}: {e}")
