30b997a95b718fd5d7f7298562fb6f6028d2a0b1
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-02-10 20:45:22 +0400

    Switch to pygame for sound playback on Linux and update requirements

--- src/sound.py
+++ src/sound.py
@@ -1,11 +1,26 @@
 from log import logger
 import threading
 import playsound
+import pygame
+import os
 
 def play(sound):
     logger.debug(f"Playing sound: {sound}")
-    try:
-        threading.Thread(target=lambda:playsound.playsound(sound), daemon=True).start()
-        logger.info(f"Sound played: {sound}")
-    except Exception as e:
-        logger.error(f"Failed to play sound {sound}: {e}")
+    if os.name == "nt": # Windows
+        try:
+            threading.Thread(target=lambda:playsound.playsound(sound), daemon=True).start()
+            logger.info(f"Sound played: {sound}")
+        except Exception as e:
+            logger.error(f"Failed to play sound {sound}: {e}")
+    if os.name == "posix": # Linux/Mac
+        try:
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound)
+            pygame.mixer.music.play()
+            logger.info(f"Sound played: {sound}")
+        except Exception as e:
+            logger.error(f"Failed to play sound {sound}: {e}")
+    """
+    I DONT KNOW WHY THIS HAPPENS BUT PLAYSOUND DOESN'T WORK ON LINUX
+    SO I SWITCHED TO PYGAME FOR LINUX. IF YOU KNOW WHY PLAYSOUND DOESN'T WORK ON LINUXPLEASE TELL ME.
+    """
